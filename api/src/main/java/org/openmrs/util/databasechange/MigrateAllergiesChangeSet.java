aff2ceeb9fffb9563d6664e8c8da2e889105af46
kbisht <kbisht@ncsu.edu>
2018-11-15 15:56:27 -0500

    Fix Sql Injection bug

--- api/src/main/java/org/openmrs/util/databasechange/MigrateAllergiesChangeSet.java
+++ api/src/main/java/org/openmrs/util/databasechange/MigrateAllergiesChangeSet.java
@@ -162,7 +162,11 @@ public class MigrateAllergiesChangeSet implements CustomTaskChange {
 		if (rs.next()) {
 			String uuid = rs.getString("property_value");
 			
-			rs = stmt.executeQuery("SELECT concept_id FROM concept WHERE uuid = '" + uuid + "'");
+			String conceptId_Query = "SELECT concept_id FROM concept WHERE uuid=?";
+			PreparedStatement preparedStatement = connection.prepareStatement(conceptId_Query);
+			preparedStatement.setString(1,uuid);
+			rs = preparedStatement.executeQuery();
+			
 			if (rs.next()) {
 				return rs.getInt("concept_id");
 			}
